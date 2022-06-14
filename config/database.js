9ad18a46e6111511443a95e0e64bf693d85f9b24
alexandrejengenhariadev <2008139@aluno.univesp.br>
2022-06-14 17:55:18 -0300

    teste

--- config/database.js
+++ config/database.js
@@ -73,12 +73,16 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      // ....
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis')
+    },
+    ssl: {
+      rejectUnauthorized: false
+    },
+    debug: Env.get('DB_DEBUG', false)
   }
-  
   
 }
