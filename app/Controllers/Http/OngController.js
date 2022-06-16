cb63f1896bf93aaf119a9c9d975e7fdee7463642
alexandrejengenhariadev <2008139@aluno.univesp.br>
2022-06-16 09:16:33 -0300

    atiualizado

--- app/Controllers/Http/OngController.js
+++ app/Controllers/Http/OngController.js
@@ -1,11 +1,24 @@
 'use strict'
+import { v4 as uuidv4 } from 'uuid'
 const Ong= use('App/Models/Ong');
 class OngController {
+      
    async store({request}){
+       validationOptions = {types: ['imagem'],
+                                  size: '2mb',}      
 
-        const dataToCreate=request.only(['nome','cep','endereco']);
-       return await Ong.create(dataToCreate);
+      const dataToCreate=request.only(['nome','cep','endereco','telefone','responsavel','email','imagem']);
+      const image = request.file('imagem', this.validationOptions)
+
+    if (image) {
+      const imageName = `${uuidv4()}.${image.extname}`
+
+      await image.move(Application.tmpPath('uploads'), {
+        name: imageName,
+      }) 
+      return await Ong.create(dataToCreate);
     }
+   }
    async list(){
      return await Ong.all();
     }
@@ -14,7 +27,7 @@ class OngController {
    }
    async update({params,request}){
       const ong = await Ong.findOrFail(params.id);
-      const dataToUpdate = request.only(['nome','cep','endereco']);
+      const dataToUpdate = request.only(['nome','cep','endereco','telefone','responsavel','email','imagem']);
       ong.merge(dataToUpdate);
       await ong.save();
       return ong;
--- database/migrations/1655380097281_ongs_schema.js
+++ database/migrations/1655380097281_ongs_schema.js
@@ -8,8 +8,12 @@ class OngsSchema extends Schema {
     this.create('ongs', (table) => {
       table.increments()
       table.string('nome',50)
-      table.integer('cep',11)
-      table.string('endereco')
+      table.string('cep',9)
+      table.string('endereco',200)
+      table.string('telefone',15)
+      table.string('responsavel',50)
+      table.string('email',50)
+      table.string('imagem')
       table.timestamps()
     })
   }
